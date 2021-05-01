6d6b255cdd41c9b96c78d477a8b898de1bd95dcb
Cameron <yeec327@gmail.com>
2021-05-01 14:32:44 -0700

    Replaced switch statement with .toLocalDateString() method to display our own format of the date

--- app.js
+++ app.js
@@ -11,33 +11,14 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   var today = new Date();
   var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: Current day number is equal to: " + currentDay)
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options)
 
   // do all of the processing in the break statements and then put it in once
   // Here, the first argument is the name of the ejs file and the second arg is the variable and value key pair
@@ -46,5 +27,5 @@ app.get("/", function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log("Server started on port 3000.");
+  console.log("Server started on port 3000!");
 });
