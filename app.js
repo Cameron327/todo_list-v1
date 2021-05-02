65fb37ea3533bfde1e230aff2f62755c1f3a5ec6
Cameron <yeec327@gmail.com>
2021-05-01 17:07:48 -0700

    Implemented EJS Layouts in addition to templates. Also, implememented multiple pages with a new list

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let ejs = require("ejs");
 const app = express();
 
 let items = ["Milk", "Eggs", "Fruit"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -28,20 +29,47 @@ app.get("/", function (req, res) {
 
   // do all of the processing in the break statements and then put it in once
   // Here, the first argument is the name of the ejs file and the second arg is the variable and value key pair
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
   // in this funciton, I used different variable names for the day to be very explicit on what is being passed to where
 });
 
 app.post("/", function (req, res) {
   let item = req.body.newItem;
-  items.push(item);
+  
+  console.log(req.body);
 
-  // use redirect for the post method and render for the get method
-  // you can't use render here because when the get method is loaded first, it won't have a value to put into the ejs variable in our html page yet
-  // in addition, you would also have to create the list variable at the top of the file and not here (for scope reasons)
-  // Use an array so that we don't just keep overriding the newest item
-  // So, ititialize it at the top with nothing
-  res.redirect("/");
+  // We have this check to see which list the item was added to
+  // Get the data from the value of our button which was named the exact same as the listTitle so that's how we know
+  if (req.body.list === "Work List"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    // use redirect for the post method and render for the get method
+    // you can't use render here because when the get method is loaded first, it won't have a value to put into the ejs variable in our html page yet
+    // in addition, you would also have to create the list variable at the top of the file and not here (for scope reasons)
+    // Use an array so that we don't just keep overriding the newest item
+    // So, ititialize it at the top with nothing
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req,res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+// Do we even need this since nothing is being posted to the work route? The only form we have is being posted to the "/" route and we do the check in there
+// Still, we do need the work get method but I don't know about the post
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+
+  res.redirect("work");
+});
+
+
+app.get("/about", function (req,res) {
+  res.render("about");
 });
 
 
